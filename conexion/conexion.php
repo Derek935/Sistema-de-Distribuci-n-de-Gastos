4654b960af9a55b7496e7cfdbe4be986c6398f82
Jose Angel Moreno Sanchez <agelms580@gmail.com>
2026-03-16 16:04:35 -0600

    se agrego la carpeta de conexion a la base de datos junto con el archivo .php para la misma

---
+++ conexion/conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+ $conn = new mysqli("208.109.17.48", "root", "1234", "actividades_direccionalmacen_db");
+
+  if ($conn->connect_error) {
+    die("Error: Imposible conectarse: " . $conn->connect_error);
+  }
+
+ echo 'Conectados a la base.<br>';
+
+ $result = $conn->query("SELECT id_cargo FROM nom_encargados");
+
+ echo "Número de registros: $result->num_rows";
+
+ $result->close();
+
+ $conn->close();
+
+  ?>
