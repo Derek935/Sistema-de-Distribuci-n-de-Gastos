627c83b884d4d40293254ae88e5515769ecf333c
Derek Serrano <derek190102@gmail.com>
2026-03-16 20:40:35 -0600

    actualizacion login

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -6,7 +6,7 @@
    $basedeDatos = "almacen_db";
 
    $conn = mysqli_connect($servidor,$usuario,$clave,$basedeDatos);
-
+    $conn->set_charset("utf8mb4");
   if ($conn->connect_error) {
     die("Error: Imposible conectarse: " . $conn->connect_error);
   }
