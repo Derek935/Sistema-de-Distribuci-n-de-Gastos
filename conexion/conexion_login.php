563024ae11374a5f9ae36bdf24cf0b46c96c0871
Derek Serrano <derek190102@gmail.com>
2026-03-16 20:57:56 -0600

    creacion conexion de login

---
+++ conexion/conexion_login.php
@@ -0,0 +1,20 @@
+<?php
+    if(!empty($_POST['login'])){
+        if(empty($_POST['usuario']) || empty($_POST['password'])){
+            echo "<script>alert('Por favor, completa todos los campos');</script>";
+        } 
+        else {
+        include("conexion/conexion.php");
+        $usuario = $_POST['usuario'];
+        $clave = $_POST['password'];
+        $sql = "SELECT * FROM nom_encargados WHERE usuario='$usuario' AND clave='$clave'";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) > 0){
+            header("Location: dashboard.html");
+            exit();
+        } else {
+            echo "<script>alert('Usuario o contraseña incorrectos');</script>";
+        }
+    }
+    }
+?>
