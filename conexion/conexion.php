bdfc571dd9928c3934c3af8f696ee00429aca52b
Derek Serrano <derek190102@gmail.com>
2026-03-17 23:37:43 -0600

    se cambio toda la conexion literalmente

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,7 +1,7 @@
 <?php
 
 
-   $conn = mysqli_connect("localhost","root","1234","almacen_db","3307");
+   $conn = mysqli_connect("localhost","root","1234","almacen_db","3306");
     $conn->set_charset("utf8mb4");
   if ($conn->connect_error) {
     die("Error: Imposible conectarse: " . $conn->connect_error);
